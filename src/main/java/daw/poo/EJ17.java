1d1eca27e75b7fb5e387f4a6daed155710e76c56
Juan José Bazán Espinosa <eltiburonjuanjo@hotmail.com>
2021-11-23 14:38:35 +0100

    23nov14

---
+++ src/main/java/daw/poo/EJ17.java
@@ -0,0 +1,17 @@
+
+package daw.poo;
+
+//17,18,19,20,24,25
+
+//Quiero una clase llamada Utilidades, sin atributos, con dos métodos: generarAleatorios 
+//en un rango dado y leer Enteros con Scanner. Luego probar en un main ambos métodos 
+//creando un objeto de la clase Utilidades
+
+public class EJ17 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
